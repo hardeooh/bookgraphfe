d502cfede57d9aaa7c04345b51471f75c497e121
hardeooh <harrydaleoh@gmail.com>
2022-09-11 15:03:36 -0700

    Progress on Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,19 +9,19 @@ const Navbar = () => {
         <a className="flex">
           <Image src="/BG_white.png" alt="Logo" width="40" height="40" className="object-contain h-10"/>
         </a>
-        <div className="flex align-middle bg-gray-400 h-10 w-3/6">
+        <div className="flex align-middle rounded-lg bg-gray-400 h-10 w-3/6">
           <MdOutlineSearch className="scale-160 mt-3 ml-3"/>
           <input type="text" className="navbar__search ml-2"/>
         </div>
       </div>
-      <div className="flex space-x-5 align-middle">
-        <a className="block text-center text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Books</a>
-        <a className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Lists</a>
-        <a className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Members</a>      
-        <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+      <div className="flex space-x-5">
+        <a className="text-white px-3 py-2 rounded-md text-lg font-medium" aria-current="page">Books</a>
+        <a className="text-white px-3 py-2 rounded-md text-lg font-medium" aria-current="page">Lists</a>
+        <a className="text-white px-3 py-2 rounded-md text-lg font-medium" aria-current="page">Members</a>      
+        <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-3 rounded">
           Sign In
         </button>
-        <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2  px-4 rounded">
+        <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2  px-3 rounded">
           Create Account
         </button>
       </div>
